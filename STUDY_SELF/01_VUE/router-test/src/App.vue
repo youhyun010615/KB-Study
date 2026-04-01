fd103669bbbc1d508d44af72aa07f1895b4b4b54
유현 <cadry238@gmail.com>
2026-04-01 23:38:43 +0900

    0401 과제 제출

---
+++ STUDY_SELF/01_VUE/router-test/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <Header />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue';
+
+export default {
+  name: 'App',
+  components: { Header },
+};
+</script>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 10px;
+}
+</style>
