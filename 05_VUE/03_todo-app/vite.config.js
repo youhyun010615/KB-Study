9122a737c7bed42e3a00a6968aca58d626316e2f
유현 <cadry238@gmail.com>
2026-04-06 16:43:05 +0900

    0406 todolist 실습

--- 05_VUE/03_todo-app/vite.config.js
+++ 05_VUE/03_todo-app/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '/@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
     },
   },
   server: {
