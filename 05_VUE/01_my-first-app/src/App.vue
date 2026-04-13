44bac107eecd891cc6822fd2134755c8094a619c
유현 <cadry238@gmail.com>
2026-04-13 22:07:09 +0900

    0413

--- 05_VUE/01_my-first-app/src/App.vue
+++ 05_VUE/01_my-first-app/src/App.vue
@@ -1,13 +1,9 @@
 <script setup>
-import ColorEmitParent from './components/02_emit/ColorEmitParent.vue';
-import VmodelEmitParent from './components/02_emit/VmodelEmitParent.vue';
-import NamedSlotParent from './components/03_slot-teleport/NamedSlotParent.vue';
-import SlotParent from './components/03_slot-teleport/slotParent.vue';
-import TeleportParent from './components/03_slot-teleport/TeleportParent.vue';
+import QuizParent from './components/05_composition-api/QuizParent.vue';
 </script>
 
 <template>
-  <TeleportParent />
+  <QuizParent />
 </template>
 
 <style scoped></style>
